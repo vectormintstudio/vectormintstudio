2820c5aeffb0171036056fff54622d922b149309
vectormintstudio <sunny@example.com>
2026-06-13 06:27:26 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+const btn = document.querySelector("button");
+
+if(btn){
+
+btn.addEventListener("click",function(){
+
+alert("Welcome to VectorMint Studio!\nLet's create something amazing.");
+
+});
+
+}
+
+});
